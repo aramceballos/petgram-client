7f5941b81c2155e4fb630cf9e58b07b79ec16e2c
Aram Ceballos <aramgonzalez12@hotmail.com>
2021-06-03 23:00:07 -0700

    :fire: Category name was removed from list of categories

--- components/Category/index.tsx
+++ components/Category/index.tsx
@@ -4,11 +4,10 @@ import { A, Image } from './styles'
 
 const DEFAULT_IMAGE = 'https://i.imgur.com/dJa0Hpl.jpg'
 
-const Category = ({ image_url = DEFAULT_IMAGE, path = '', category = '' }) => (
+const Category = ({ image_url = DEFAULT_IMAGE, path = '' }) => (
   <Link href={path}>
     <A>
       <Image src={image_url} alt="category image" />
-      {category}
     </A>
   </Link>
 )
