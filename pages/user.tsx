17290499cd5baff2059f3aa2ceb0989d0730eb8c
Aram Ceballos <aramgonzalez12@hotmail.com>
2021-06-17 19:42:47 -0700

    :sparkles: Grid of posts now is shown in user profile

--- pages/user.tsx
+++ pages/user.tsx
@@ -2,12 +2,18 @@ import React, { useEffect, useState } from 'react'
 import { IncomingMessage, ServerResponse } from 'http'
 import Cookies from 'cookies'
 import Image from 'next/image'
+import Link from 'next/link'
 import styled from 'styled-components'
 import { useCookies } from 'react-cookie'
+import axios from 'axios'
 
 import Layout from '../components/Layout'
 
 const Container = styled.div`
+  padding-bottom: 70px;
+`
+
+const UserContainer = styled.div`
   display: flex;
   margin: 16px;
 `
@@ -36,14 +42,37 @@ const Name = styled.h1`
   font-size: 14px;
 `
 
+const PostsContainer = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 3px;
+`
+
+const ImageWrapper = styled.div`
+  width: 100%;
+  position: relative;
+  padding-top: 100%;
+`
+
+const Img = styled.img`
+  object-fit: cover;
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+`
+
 const Button = styled.button`
   border-radius: 3px;
   color: #d80000;
   height: 32px;
   display: block;
-  width: 100%;
+  width: 90%;
   text-align: center;
   border: 1px solid #c7c7c7;
+  margin: 20px auto 0;
 
   &[disabled] {
     opacity: 0.3;
@@ -78,11 +107,14 @@ export const getServerSideProps = async ({
 }
 
 const User = () => {
-  const [, setCookie] = useCookies(['t'])
+  const [token, setCookie] = useCookies(['t'])
+  const [userId, setUserId] = useState<string | null>('')
   const [username, setUsername] = useState<string | null>('')
   const [name, setName] = useState<string | null>('')
+  const [userPosts, setUserPosts] = useState<IPost[]>([])
 
   useEffect(() => {
+    setUserId(window.localStorage.getItem('userInfo-id'))
     setUsername(window.localStorage.getItem('userInfo-username'))
     setName(window.localStorage.getItem('userInfo-name'))
     if (
@@ -98,6 +130,10 @@ const User = () => {
     }
   }, [])
 
+  useEffect(() => {
+    userId && getPosts()
+  }, [userId])
+
   const handleClick = () => {
     window.localStorage.removeItem('userInfo-id')
     window.localStorage.removeItem('userInfo-name')
@@ -106,10 +142,30 @@ const User = () => {
     window.location.reload()
   }
 
+  const getPosts = async () => {
+    try {
+      const res = await axios(`http://localhost:5000/api/p?user_id=${userId}`, {
+        headers: {
+          Authorization: `Bearer ${token.t}`,
+        },
+      })
+      setUserPosts(res.data.data)
+    } catch (error) {
+      if (
+        error.response?.data?.message === 'Missing or malformed JWT' ||
+        error.response?.data?.message === 'Invalid or expired JWT'
+      ) {
+        setCookie('t', '')
+        window.location.reload()
+      }
+      console.error(error.response?.data?.message)
+    }
+  }
+
   return (
     <Layout title="User">
-      <div>
-        <Container>
+      <Container>
+        <UserContainer>
           <ImageContainer>
             <Image
               height={77}
@@ -120,12 +176,22 @@ const User = () => {
           <section>
             <Username>{username}</Username>
           </section>
-        </Container>
+        </UserContainer>
         <NameContainer>
           <Name>{name}</Name>
         </NameContainer>
+        <PostsContainer>
+          {userPosts.length > 0 &&
+            userPosts.map((post) => (
+              <ImageWrapper key={post.id}>
+                <Link href={`/post/${post.id}`}>
+                  <Img src={post.image_url} alt={post.description} />
+                </Link>
+              </ImageWrapper>
+            ))}
+        </PostsContainer>
         <Button onClick={handleClick}>Log out</Button>
-      </div>
+      </Container>
     </Layout>
   )
 }
