6d43ff25df78982959960765f0e619492864d62a
Aram Ceballos <aramgonzalez12@hotmail.com>
2021-06-19 11:52:06 -0700

    :lipstick: NavBar icons changed

--- components/NavBar/styles.tsx
+++ components/NavBar/styles.tsx
@@ -1,13 +1,17 @@
 import styled from 'styled-components'
 import LinkRouter from 'next/link'
 
+type TImageContianer = {
+  showBorder: boolean
+}
+
 export const Nav = styled.nav`
   align-items: center;
   background: #fcfcfc;
   border-top: 1px solid #e0e0e0;
   bottom: 0;
   display: flex;
-  height: calc(50px + env(safe-area-inset-bottom));
+  height: calc(44px + env(safe-area-inset-bottom));
   justify-content: space-around;
   left: 0;
   margin: 0 auto;
@@ -26,4 +30,19 @@ export const Link = styled(LinkRouter)`
   justify-content: center;
   text-decoration: none;
   width: 100%;
+`
+
+export const ImageContainer = styled.div<TImageContianer>`
+  border-radius: 50%;
+  border: ${({ showBorder }) =>
+    showBorder ? '1px solid #000' : '1px solid #0000'};
+  width: 28px;
+  height: 28px;
+  padding: 1px;
+`
+
+export const Img = styled.img`
+  border-radius: 50%;
+  width: 24px;
+  height: 24px;
 `
